f59ceb03b6ba27aecc3a944ec5ef2ef93bf8f8d8
Andrew Haines <andrew.haines@red-badger.com>
2017-02-10 11:27:37 +0000

    Use test2junit for comparison

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :url "https://github.com/redbadger/test-report-junit-xml-example"
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:test {:plugins [[lein-test-report-junit-xml "0.2.0"]]}})
+  :plugins [[test2junit "1.2.5"]])
